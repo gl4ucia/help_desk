b4aa83a05c6bebcf93f09916bf05e3e957035986
gl4ucia <contatoglauciam@gmail.com>
2019-11-20 18:22:57 -0300

    Teste

--- src/main/java/br/com/maven/hd/beans/HistoricoBeans.java
+++ src/main/java/br/com/maven/hd/beans/HistoricoBeans.java
@@ -10,6 +10,50 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="pessoa")
+@Table(name="Historico")
 public class HistoricoBeans {
 	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name ="h_id")
+private Long h_id;
+@Column(name ="h_ordemservico")
+private Long h_ordemservico;
+@Column(name ="h_funcionario")
+private Long h_funcionario;
+@Column(name ="h_status")
+private Long h_status;
+@Column(name ="h_data")
+private Long h_data;
+
+	public Long getH_id() {
+		return h_id;
+	}
+	public void setH_id(Long h_id) {
+		this.h_id = h_id;
+	}
+	public Long getH_ordemservico() {
+		return h_ordemservico;
+	}
+	public void setH_ordemservico(Long h_ordemservico) {
+		this.h_ordemservico = h_ordemservico;
+	}
+	public Long getH_funcionario() {
+		return h_funcionario;
+	}
+	public void setH_funcionario(Long h_funcionario) {
+		this.h_funcionario = h_funcionario;
+	}
+	public Long getH_status() {
+		return h_status;
+	}
+	public void setH_status(Long h_status) {
+		this.h_status = h_status;
+	}
+	public Long getH_data() {
+		return h_data;
+	}
+	public void setH_data(Long h_data) {
+		this.h_data = h_data;
+	}
+}
